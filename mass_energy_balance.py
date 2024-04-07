59be93197588ee55915e8fbd89de0df0ff16539e
Yadpreet Cheema <yadpreetcheema99@gmail.com>
2024-04-07 11:56:10 -0700

    vectorized calculations

--- mass_energy_balance.py
+++ mass_energy_balance.py
@@ -1,26 +1,31 @@
 import pandas as pd
 import numpy as np
 import sympy as sp
+import matplotlib.pyplot as plt
+from scipy.integrate import quad
 # create dataframe with boiler 2 data
 df = pd.read_csv('B2_Cleaned_CEC_Data_2021.csv')
 
 
-b2_row=df.iloc[0] #first row of data for boiler 2
+
 
 
 
 #*********Mass Balance********
 
 #***Determine the molar flow rate of fuel at operating conditions***
+
 #variables 
-fuel_volumetric_flowrate=b2_row['Campus Energy Centre Boiler B-2 Gas Flow Rate (m³/h)'] #m³/h
-fuel_pressure=b2_row['Campus Energy Centre Boiler B-2 Gas Pressure (kPa)'] #kPa
+fuel_volumetric_flowrate=df['Campus Energy Centre Boiler B-2 Gas Flow Rate (m³/h)'] #m³/h
+fuel_pressure=df['Campus Energy Centre Boiler B-2 Gas Pressure (kPa)'] #kPa
 R= 8.314 #L*kPa/mol*K
-T=b2_row['UBC Temp (°C)'] #°C
+T=df['UBC Temp (°C)'] #°C
+
 #use P*V=n*R*T to calculate molar flowrate
 def molar_flowrate(P,V,R,T):
     n=(P*V)/(R*T)
     return n
+
 #unit conversions
 fuel_volumetric_flowrate=fuel_volumetric_flowrate*(1000/1) #m³/h -> L/h
 T = T + 273.15 # °C -> K
@@ -31,8 +36,8 @@ print(f'Molar flowrate fuel in (moles/h): {molar_flowrate(fuel_pressure, fuel_vo
 
 #***Determine the mole fractions of wet air***
 #variables 
-rel_humidity=b2_row['UBC Humidity (%RH)']/100 #relative humidity as fraction
-T=b2_row['UBC Temp (°C)'] #°C
+rel_humidity=df['UBC Humidity (%RH)']/100 #relative humidity as fraction
+T=df['UBC Temp (°C)'] #°C
 P_atmosphere=101325# Pascals
 A= 5.40221 #antoine A constant water (	Bridgeman and Aldrich, 1964	)
 B= 1838.675 #antoine B constant water (	Bridgeman and Aldrich, 1964	)
@@ -92,7 +97,7 @@ CO2_produced = CO2_coeff_CH4 * flow_rate_CH4 + CO2_coeff_C2H6 * flow_rate_C2H6
 H2O_produced = H2O_coeff_CH4 * flow_rate_CH4 + H2O_coeff_C2H6 * flow_rate_C2H6
 
 # Define the percentage of O2 in the stack
-fraction_O2_stack = b2_row['Campus Energy Centre Boiler B-2 Exhaust O2 (%)']/100 #from data set for boiler 2
+fraction_O2_stack = df['Campus Energy Centre Boiler B-2 Exhaust O2 (%)']/100 #from data set for boiler 2
 
 #Note: y_H2O , y_N2, _O2 are from previous calculations and represent mole fractions of inlet air stream
 
@@ -100,20 +105,22 @@ fraction_O2_stack = b2_row['Campus Energy Centre Boiler B-2 Exhaust O2 (%)']/100
 A_in = (fraction_O2_stack * (O2_consumed - H2O_produced - CO2_produced) - O2_consumed) / (fraction_O2_stack * (y_H2O_air + y_N2_air + y_O2_air) - y_O2_air)
 print(f'Molar flow rate Air in (mol/h): {A_in}')
 
-#Determine molar flow rate of flue gas (N_flue (mol/h))
-N_flue= (A_in*y_O2_air-O2_consumed)+(A_in*y_N2_air)+(A_in*y_H2O_air+H2O_produced)+CO2_produced
+#Determine molar flow rate of flue gas (G (mol/h))
+G = (A_in*y_O2_air-O2_consumed)+(A_in*y_N2_air)+(A_in*y_H2O_air+H2O_produced)+CO2_produced
 print(f'Molar flowrate flue gas out(mol/h)')
 #determine molar flow rates of components in flue gas (mol/h)
-N_O2_flue= A_in*y_O2_air-O2_consumed
-N_N2_flue=A_in*y_N2_air
-N_H2O_flue=A_in*y_H2O_air+H2O_produced
-N_CO2_flue=CO2_produced
+G_O2_flue= A_in*y_O2_air-O2_consumed
+G_N2_flue=A_in*y_N2_air
+G_H2O_flue=A_in*y_H2O_air+H2O_produced
+G_CO2_flue=CO2_produced
 
 #determine mole fractions of components in flue glass
-y_O2_flue= N_O2_flue/N_flue
-y_N2_flue= N_N2_flue/N_flue
-y_H2O_flue= N_H2O_flue/N_flue
-y_CO2_flue= N_CO2_flue/N_flue
+y_O2_flue= G_O2_flue/G
+y_N2_flue= G_N2_flue/G
+y_H2O_flue= G_H2O_flue/G
+y_CO2_flue= G_CO2_flue/G
+
+
 
 print(f'Flue gas mole fraction O2, N2, H2O, CO2: {y_O2_flue, y_N2_flue, y_H2O_flue,y_CO2_flue }')
 
@@ -128,17 +135,20 @@ print(f'Flue gas mole fraction O2, N2, H2O, CO2: {y_O2_flue, y_N2_flue, y_H2O_fl
 
 #variables
 T_ref=298.15 #reference temperature in kelvin
-water_flowrate=b2_row['Campus Energy Centre Boiler B-2 Water Flow Rate (L/s)'] #water flow rate (L/s)
-cw_T_in=b2_row['Campus Energy Centre Boiler B-2 Entering Water Temp (°C)'] + 273.15 #cold water inlet temperature (K)
-hw_T_out=b2_row['Campus Energy Centre Boiler B-2 Leaving Water Temp (°C)'] + 273.15 #hot water outlet temperature (K)
+water_flowrate=df['Campus Energy Centre Boiler B-2 Water Flow Rate (L/s)'] #water flow rate (L/s)
+cw_T_in=df['Campus Energy Centre Boiler B-2 Entering Water Temp (°C)'] + 273.15 #cold water inlet temperature (K)
+hw_T_out=df['Campus Energy Centre Boiler B-2 Leaving Water Temp (°C)'] + 273.15 #hot water outlet temperature (K)
 
 
 
-#return enthalpy in J/mol
-def calculate_enthalpy(a,b,c,d,T_f, T_reference):
-    T = sp.symbols('x')
-    f = a + b*T + c*T**2 + d*T**3
-    enthalpy = sp.integrate(f, (T, T_reference, T_f))
+# Define a vectorized enthalpy calculation function
+def calculate_enthalpy(a, b, c, d, T_f, T_reference = 298.15):
+    # Define the internal function for integration
+    def f(T, a, b, c, d):
+        return a + b*T + c*T**2 + d*T**3
+
+    # Perform numerical integration for each element of T_f
+    enthalpy = np.array([quad(f, T_ref, T, args=(a, b, c, d))[0] for T in T_f])
     return enthalpy
 
 #enthalpy (J/mol) , molar_flow (mol/s)
@@ -176,7 +186,6 @@ print(f'Heat transfer from hot water outlet stream(J/s): {q_hw}')
 
 
 
-
 #Determine heat transfer from fuel stream (q_f) , fuel composition: 95% CH4, 5% C2H6
 
 ##heat capacity coefficients
@@ -195,7 +204,7 @@ y_CH4=0.95
 y_C2H6=0.05
 
 #assume fuel temperature is ambient temperature
-T_fuel= b2_row['UBC Temp (°C)'] +273.15 #Kelvins
+T_fuel= df['UBC Temp (°C)'] +273.15 #Kelvins
 
 #calculate enthalpy of fuel with equation: H_fuel = y_CH4 * H_CH4 + y_C2H6 * H_C2H6
 H_CH4 = calculate_enthalpy(a_CH4, b_CH4, c_CH4, d_CH4, T_fuel, T_ref)
@@ -234,8 +243,8 @@ print(f'Heat of combustion of fuel J/mol: {H_combustion_fuel}')
 #Determine heat transfer of fuel, use equation: q_F=F*{Cp_F*(T_F – T_o) + H_cF} = F * {H_F + H_cF }
 
 #calculate fuel molar flow rate (mol/s)
-fuel_volumetric_flowrate= b2_row['Campus Energy Centre Boiler B-2 Gas Flow Rate (m³/h)'] * (1000/1) * 3600 # L/s
-fuel_pressure = b2_row['Campus Energy Centre Boiler B-2 Gas Pressure (kPa)'] 
+fuel_volumetric_flowrate= df['Campus Energy Centre Boiler B-2 Gas Flow Rate (m³/h)'] * (1000/1) / 3600 # L/s
+fuel_pressure = df['Campus Energy Centre Boiler B-2 Gas Pressure (kPa)'] 
 R= 8.314 #L*kPa/mol*K
 
 #use P*V=n*R*T to calculate molar flowrate
@@ -245,8 +254,8 @@ def molar_flowrate(P,V,R,T):
 
 F = molar_flowrate(fuel_pressure, fuel_volumetric_flowrate, R, T_fuel) #fuel molar flow rate (mol/s)
 
-qF = F * (H_fuel + H_combustion_fuel)
-print(f'Heat transfer of fuel (J/s): {qF}')
+q_F = F * (H_fuel + H_combustion_fuel)
+print(f'Heat transfer of fuel (J/s): {q_F}')
 
 
 
@@ -269,7 +278,7 @@ b_N2 = -0.01357
 c_N2 = 2.68E-6
 d_N2 = -1.168E-8
 
-T_air= b2_row['UBC Temp (°C)'] +273.15 #air temperature (K)
+T_air= df['UBC Temp (°C)'] +273.15 #air temperature (K)
 
 H_O2 = calculate_enthalpy(a_O2, b_O2, c_O2, d_O2, T_air, T_ref)
 
@@ -328,7 +337,7 @@ y_CO2_flue_dry = y_CO2_flue/ ( y_O2_flue + y_N2_flue + y_CO2_flue )
 #mole fraction of water vapor in flue gas
 
 
-T_flue = b2_row['Campus Energy Centre Boiler B-2 Exhaust Temp (°C)'] + 273.15 #flue gas temp (K)
+T_flue = df['Campus Energy Centre Boiler B-2 Exhaust Temp (°C)'] + 273.15 #flue gas temp (K)
 H_CO2 = calculate_enthalpy(a_CO2, b_CO2, c_CO2, d_CO2, T_flue, T_ref)
 H_flue_dry = y_CO2_flue_dry * H_CO2 + y_N2_flue_dry * H_N2 + y_O2_flue_dry * H_O2
 
@@ -339,3 +348,129 @@ H_flue_H2O = calculate_enthalpy(a_H2O_vapor, b_H2O_vapor, c_H2O_vapor, d_H2O_vap
 #Determine enthalpy of flue gas
 H_flue = ( y_O2_flue + y_N2_flue + y_CO2_flue ) * H_flue_dry  + y_H2O_flue * (H_flue_H2O)
 print(f'Enthalpy of flue gas (J/mol): {H_flue}')
+
+
+
+#Determine heat transfer of dry flue gas
+
+G_dry = G * (y_O2_flue + y_N2_flue + y_CO2_flue) #molar flow rate dry flue gas (mol/h)
+
+q_flue_dry = G_dry * H_flue_dry #heat transfer dry flue gas (J/mol)
+
+
+
+#Determine heat transfer wet flue gas
+H_H2O_vap = 44000 #enthalpy vaporization water at 25 celcius (J/mol)
+G_H2O = G * (y_H2O_flue) #molar flow rate wet flue gas (mol/h)
+
+q_flue_H2O = G_H2O * ( H_flue_H2O + H_H2O_vap ) #heat transfer wet flue gas (J/mol)
+
+#Determine heat transfer flue gas
+q_flue = (q_flue_dry + q_flue_H2O)/3600 #heat transfer flue gas (J/s)
+
+print(f'heat transfer flue gas (J/s): {q_flue}')
+
+
+#Determine energy loss of system (q_loss)
+#use equation: q_loss = energy_in - energy_out
+E_in = q_F + q_cw + q_A # energy in (J/s) 
+E_out = q_flue + q_hw #energy out (J/s)
+
+q_loss = E_in - E_out
+print(f'heat loss (J/s): {q_loss}')
+
+
+
+
+#***Analysis questions***
+
+#***flue gas flow rate
+df['Campus Energy Centre Boiler B-2 Gas Flow Rate (kmol/h)'] = G/1000
+Q1 = df['Campus Energy Centre Boiler B-2 Gas Flow Rate (kmol/h)'].quantile(0.25)
+Q3 = df['Campus Energy Centre Boiler B-2 Gas Flow Rate (kmol/h)'].quantile(0.75)
+IQR = Q3 - Q1
+
+# Define bounds for outliers
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+
+# Filter out the outliers and negative values
+filtered_df = df[(df['Campus Energy Centre Boiler B-2 Gas Flow Rate (kmol/h)'] >= lower_bound) & 
+                 (df['Campus Energy Centre Boiler B-2 Gas Flow Rate (kmol/h)'] <= upper_bound) &
+                 (df['Campus Energy Centre Boiler B-2 Gas Flow Rate (kmol/h)'] > 0)]
+
+# Plotting
+plt.scatter(filtered_df.index, filtered_df['Campus Energy Centre Boiler B-2 Gas Flow Rate (kmol/h)'])
+plt.xlabel("Time")
+plt.ylabel("Flowrate of Flue Gas (kmol/hr)")
+plt.title("Flue Gas Flow Rate (kmol/hr)")
+plt.show()
+
+#***flue gas composition
+# Plotting
+df['mole fraction O2 flue gas'] = y_O2_flue
+df['mole fraction N2 flue gas'] = y_N2_flue
+df['mole fraction H2O flue gas'] = y_H2O_flue
+df['mole fraction CO2 flue gas'] = y_CO2_flue
+plt.figure(figsize=(10, 6))
+
+# Filter out values below zero using np.maximum
+plt.plot(df.index, np.maximum(df['mole fraction O2 flue gas'], 0), label='O2 Mole Fraction')
+plt.plot(df.index, np.maximum(df['mole fraction N2 flue gas'], 0), label='N2 Mole Fraction')
+plt.plot(df.index, np.maximum(df['mole fraction H2O flue gas'], 0), label='H2O Mole Fraction')
+plt.plot(df.index, np.maximum(df['mole fraction CO2 flue gas'], 0), label='CO2 Mole Fraction')
+
+plt.xlabel("Time")
+plt.ylabel("Mole Fraction")
+plt.title("Mole Fractions of Components in Flue Gas")
+plt.legend()
+plt.grid(True)
+plt.show()
+
+
+#***Plot the energy transfer rate for the boiler over time – hot water, cold water, heat loss
+df['cold water heat transfer kJ/s'] = q_cw / 1000
+df['hot water heat transfer kJ/s'] = q_hw / 1000
+df['heat loss furnace/boiler kJ/s'] = q_loss / 1000
+
+plt.figure(figsize=(12, 8))
+
+def filter_outliers(df, column_name):
+    Q1 = df[column_name].quantile(0.25)
+    Q3 = df[column_name].quantile(0.75)
+    IQR = Q3 - Q1
+
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+
+    # Filter the DataFrame
+    filtered = df[(df[column_name] >= lower_bound) & (df[column_name] <= upper_bound)]
+    return filtered
+
+# Filtering each column individually
+df['cold water heat transfer kJ/s'] = q_cw / 1000
+df['hot water heat transfer kJ/s'] = q_hw / 1000
+df['heat loss furnace/boiler kJ/s'] = q_loss / 1000
+
+filtered_cold_water = filter_outliers(df, 'cold water heat transfer kJ/s')
+filtered_hot_water = filter_outliers(df, 'hot water heat transfer kJ/s')
+filtered_heat_loss = filter_outliers(df, 'heat loss furnace/boiler kJ/s')
+
+plt.plot(filtered_cold_water.index, filtered_cold_water['cold water heat transfer kJ/s'], label='Cold Water Heat Transfer (kJ/s)')
+plt.plot(filtered_hot_water.index, filtered_hot_water['hot water heat transfer kJ/s'], label='Hot Water Heat Transfer (kJ/s)')
+plt.plot(filtered_heat_loss.index, filtered_heat_loss['heat loss furnace/boiler kJ/s'], label='Heat Loss Furnace/Boiler (kJ/s)')
+
+plt.xlabel("Time or Index")
+plt.ylabel("Energy Transfer Rate (kJ/s)")
+plt.title("Energy Transfer Rates for Boiler Over Time (kJ/s)")
+plt.legend()
+plt.grid(True)
+plt.show()
+
+
+
+
+
+
+
+#******Boiler efficiency*******
